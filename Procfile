603309f4e196a43a6f5db2407b22b42ff64999b2
iDennisofficial <dennisdamas100@gmail.com>
2025-02-24 09:36:00 +0300

    Update

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: gunicorn studybuddy.wsgi
+
+web: gunicorn studybuddy.wsgi --log-file - 
+#or works good with external database
+web: python manage.py migrate && gunicorn studybuddy.wsgi
--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -48,7 +48,7 @@ AUTH_USER_MODEL = "base.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -139,7 +139,7 @@ MEDIA_URL = "userprofilepicture/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR / 'static')
 ]
 
 MEDIA_ROOT = BASE_DIR / "static/images/userprofilepicture"
